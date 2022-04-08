2aca1245bc66ab57016abde187a4a649366efc4b
Shahrukh-10 <56799965+Shahrukh-10@users.noreply.github.com>
2022-04-08 18:07:58 +0530

    Add files via upload

---
+++ src/components/main/DescreptionComp.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./descreptionComp.css"
+const DescreptionComp = () => {
+  return (
+    <div className="main2">
+        <div className="components">
+        <div className="row1">
+            <div className="comp">One</div>
+            <div className="comp">One</div>
+            <div className="comp">One</div>
+
+        </div>
+        <div className="row2">
+
+            <div className="comp">One</div>
+            <div className="comp">One</div>
+            <div className="comp">One</div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default DescreptionComp
